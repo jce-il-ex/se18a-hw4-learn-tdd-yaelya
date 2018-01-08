fcf738a1d3ee91d55b26a6d5af14f569a8c97edd
Yael Yazdi <yael.yazdi96@gmail.com>
2018-01-08 18:11:39 +0200

    red  getChange(486,1000) should equal [500, 10, 2, 2]:, sapir

--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@ var C = require('../cash.js');  // our module
 it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
 })
+it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+})
 
 describe('Cash Register', function(){
   describe('Module C', function(){
