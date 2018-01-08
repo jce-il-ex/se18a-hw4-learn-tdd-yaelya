138b7cd11fa3c771f9ba184e64f02b196235bda7
Yael Yazdi <yael.yazdi96@gmail.com>
2018-01-08 18:03:44 +0200

    red  getChange(210,300) should equal [50,20,20]:

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var assert = require("assert"); // core module
 var C = require('../cash.js');  // our module
-
+it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
 describe('Cash Register', function(){
   describe('Module C', function(){
     it('should have a getChange Method', function(){
