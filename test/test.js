10e8f865f428e93e1cadf9d3cc956288f7363b73
Yael Yazdi <yael.yazdi96@gmail.com>
2018-01-08 18:06:10 +0200

    red  getChange(210,300) should equal [50,20,20]: AssertionError: true deepEqual [ 50, 20, 20 ], sapir

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var C = require('../cash.js');  // our module
 it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
 })
+
 describe('Cash Register', function(){
   describe('Module C', function(){
     it('should have a getChange Method', function(){
