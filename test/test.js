8e6e7d14f0c3f30ba91da5d8af7813fd6f675079
Yael Yazdi <yael.yazdi96@gmail.com>
2018-01-08 18:20:50 +0200

    green one more test to ensure we are fully exercising our method, yael

--- test/test.js
+++ test/test.js
@@ -6,6 +6,9 @@ it('getChange(210,300) should equal [50,20,20]', function(){
 it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
     assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
 })
+it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+    assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
+});
 
 describe('Cash Register', function(){
   describe('Module C', function(){
